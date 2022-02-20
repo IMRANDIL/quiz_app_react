c339eff5279aa03994034aaecbc5c217c03d914f
IMRANDIL <aliimranadil2@gmail.com>
2022-02-20 17:37:08 +0530

    yh

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            Made with Love <span role='img' aria-label='love'>💝</span> by IMRANDIL
+        <div style={{ textAlign: 'center' }}>
+            Made with Love <span role='img' aria-label='love'>💝</span> by <a href='https://github.com/IMRANDIL' style={{ cursor: 'pointer', color: 'firebrick' }} target='_blank' rel="noopener noreferrer">IMRANDIL</a>
         </div>
     )
 }
