f95949f6f7c093283334e36a1bcf882aa832c5c1
IMRANDIL <aliimranadil2@gmail.com>
2022-02-20 18:32:00 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Home from './Pages/Home/Home';
+import Quiz from './Pages/Quiz/Quiz'
+import Result from './Pages/Result/Result'
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App" style={{ backgroundImage: 'url(./ques1.png)' }}>
         <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/quiz' element={<Quiz />} />
+          <Route path='/result' element={<Result />} />
+        </Routes>
       </div>
       <Footer />
     </BrowserRouter>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: 'center', marginBottom: 10 }}>
             Made with Love <span role='img' aria-label='love'>💝</span> by <a href='https://github.com/IMRANDIL' style={{ cursor: 'pointer', color: 'firebrick' }} target='_blank' rel="noopener noreferrer">IMRANDIL</a>
         </div>
     )
