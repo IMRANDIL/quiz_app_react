d35d4f9c8d84e94079d9b4c561c0ac0ac743ecc5
IMRANDIL <aliimranadil2@gmail.com>
2022-02-21 08:25:37 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ import Result from './Pages/Result/Result'
 function App() {
 
   const [name, setName] = useState('');
-  const [questions, setQuestions] = useState('');
+  const [questions, setQuestions] = useState([]);
   const [score, setScore] = useState(0)
 
 
-  const fetchQuestions = async (category, difficulty) => {
-    const data = await axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`);
-    console.log(data);
+  const fetchQuestions = async (category = '', difficulty = '') => {
+    const { data } = await axios.get(`https://opentdb.com/api.php?amount=10${category && `&category=${category}`}${difficulty && `&difficulty=${difficulty}`}&type=multiple`);
+    setQuestions(data.results)
   }
 
 
@@ -33,7 +33,7 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home name={name} setName={setName} fetchQuestions={fetchQuestions} />} />
-          <Route path='/quiz' element={<Quiz />} />
+          <Route path='/quiz' element={<Quiz name={name} questions={questions} score={score} setScore={setScore} />} />
           <Route path='/result' element={<Result />} />
         </Routes>
       </div>
